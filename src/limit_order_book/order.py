2c1169660c439df1cdc0828f90f47669537ae937
Alex Smith <189167473+ahs5@users.noreply.github.com>
2026-09-14 22:45:12 +0100

    fill_order inequality fixes

--- src/limit_order_book/order.py
+++ src/limit_order_book/order.py
@@ -4,10 +4,7 @@ class Side(Enum):
     BUY = "BUY"
     SELL = "SELL"
 
-
-
 class Order:
-
     def __init__(self, order_id, side, price, quantity):
         self.order_id = order_id
         self.side = side
@@ -16,8 +13,10 @@ class Order:
         self.remaining_quantity = quantity
         self.timestamp = None
 
-    def fill_order(self, quantity):
-        if quantity > 0 and quantity <= self.remaining_quantity:
-            self.remaining_quantity -= quantity
-        else:
+    def fill_order(self, fill_quantity):
+        if 0 < fill_quantity <= self.remaining_quantity:
+            self.remaining_quantity -= fill_quantity
+        elif fill_quantity <= 0:
             raise ValueError("quantity must be greater than 0")
+        elif fill_quantity > self.remaining_quantity:
+            raise ValueError("quantity must be less than remaining quantity")
