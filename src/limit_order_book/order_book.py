1253bd70c65a73fbdbee5f5316be99c83fd2e28d
Alex Smith <189167473+ahs5@users.noreply.github.com>
2026-09-15 00:40:32 +0100

    order book created

---
+++ src/limit_order_book/order_book.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+from limit_order_book.order import Side
+
+
+class OrderBook:
+    def __init__(self):
+        self.bids = dict()
+        self.asks = dict()
+
+    def add_order(self, order):
+        if order.side == Side.BUY:
+            if order.price in self.bids:
+                self.bids[order.price].append(order)
+            else:
+                self.bids[order.price] = deque([order])
+
+        if order.side == Side.SELL:
+            if order.price in self.asks:
+                self.asks[order.price].append(order)
+            else:
+                self.asks[order.price] = deque([order])
+
+        else:
+            raise ValueError("order side must be BUY or SELL")
