c917b0bc36e6b83a8905ba9c068a9c8e57082749
Alex Smith <189167473+ahs5@users.noreply.github.com>
2026-09-15 01:13:45 +0100

    methods for best bid and asks created

--- src/limit_order_book/order_book.py
+++ src/limit_order_book/order_book.py
@@ -14,12 +14,22 @@ class OrderBook:
                 self.bids[order.price].append(order)
             else:
                 self.bids[order.price] = deque([order])
-
-        if order.side == Side.SELL:
+        elif order.side == Side.SELL:
             if order.price in self.asks:
                 self.asks[order.price].append(order)
             else:
                 self.asks[order.price] = deque([order])
-
         else:
             raise ValueError("order side must be BUY or SELL")
+
+    def best_bid(self):
+        if self.bids:
+            return self.bids[(max(self.bids))][0]
+        else:
+            return None
+
+    def best_ask(self):
+        if self.asks:
+            return self.asks[(min(self.asks))][0]
+        else:
+            return None
